aaf73b437577c0e44d91ce573461073995e1ffde
Andy Kreling <krelinga@gmail.com>
2024-11-12 06:46:19 +0000

    pull ripper smb password from bitwarden.

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+set -e
+
+echo "Installing ansible-galaxy dependencies..."
+ansible-galaxy install -r requirements.yaml
+echo
+echo "Installing bitwarden python sdk..."
+pipx inject ansible-core bitwarden-sdk
