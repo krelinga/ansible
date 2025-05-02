b44bd919b17f2fa160a36bcc16654fdd662f1305
Andy Kreling <krelinga@gmail.com>
2025-05-02 04:02:20 +0000

    get a client connection to truenas sort of working.
    auth is a TODO, but at least the error looks right.

--- library/truenas_cert_upload.py
+++ library/truenas_cert_upload.py
@@ -1,10 +1,18 @@
 #! /usr/local/bin/python
 
 from ansible.module_utils.basic import AnsibleModule
+from truenas_api_client import Client
 
 def main():
     module = AnsibleModule(argument_spec=dict())
-    module.exit_json(changed=False, msg="Dummy module executed")
+    msg = "failed to use client"
+
+    with Client("wss://truenas.i.krel.ing/websocket", verify_ssl=False) as c:
+        resp = c.call("system.general.country_choices")
+        msg = str(resp)
+
+    module.exit_json(changed=False, msg=msg)
+
 
 if __name__ == '__main__':
     main()
