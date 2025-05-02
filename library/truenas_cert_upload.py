ddd2ff5cf15c368cbade04e73fdee8c1d49dae77
Andy Kreling <krelinga@gmail.com>
2025-05-02 04:03:45 +0000

    require api_key and api_user arguments.

--- library/truenas_cert_upload.py
+++ library/truenas_cert_upload.py
@@ -4,7 +4,12 @@ from ansible.module_utils.basic import AnsibleModule
 from truenas_api_client import Client
 
 def main():
-    module = AnsibleModule(argument_spec=dict())
+    module = AnsibleModule(
+        argument_spec=dict(
+            api_user=dict(type='str', required=True),
+            api_key=dict(type='str', required=True)
+        )
+    )
     msg = "failed to use client"
 
     with Client("wss://truenas.i.krel.ing/websocket", verify_ssl=False) as c:
