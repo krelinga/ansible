175802b821b3f1dcbbc99dc6db3d3200135e6394
Andy Kreling <krelinga@gmail.com>
2025-05-02 04:36:40 +0000

    get a simple API call to truenas working and print the result.

--- library/truenas_cert_upload.py
+++ library/truenas_cert_upload.py
@@ -6,13 +6,15 @@ from truenas_api_client import Client
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            api_user=dict(type='str', required=True),
             api_key=dict(type='str', required=True)
         )
     )
     msg = "failed to use client"
 
+    # https://forums.truenas.com/t/getting-error-trying-to-connect-via-truenas-api-client-to-websockets-api-endpoint-api-current/28152
     with Client("wss://truenas.i.krel.ing/websocket", verify_ssl=False) as c:
+        resp = c.call("auth.login_with_api_key", module.params['api_key'])
+        assert resp
         resp = c.call("system.general.country_choices")
         msg = str(resp)
 
