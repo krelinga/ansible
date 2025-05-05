266cecac0a156b130ab0a412fdd04d6678c7a081
Andy Kreling <krelinga@gmail.com>
2025-05-05 03:31:10 +0000

    query opnsense certs.

--- library/opnsense_cert_upload.py
+++ library/opnsense_cert_upload.py
@@ -2,6 +2,9 @@
 
 from ansible.module_utils.basic import AnsibleModule
 
+import requests
+from requests.auth import HTTPBasicAuth
+
 def main():
     module = AnsibleModule(
         argument_spec=dict(
@@ -19,7 +22,16 @@ def main():
     api_secret = module.params['api_secret']
     host = module.params['host']
 
-    module.exit_json(changed=False)
+
+    uri = f"https://{host}/api/trust/cert/search"
+    try:
+        response = requests.get(uri, auth=HTTPBasicAuth(api_key, api_secret), verify=False)
+        if response.status_code == 200:
+            module.exit_json(changed=False, response=response.json())
+        else:
+            module.fail_json(msg=f"HTTP GET request failed with status code {response.status_code}", response=response.json())
+    except requests.exceptions.RequestException as e:
+        module.fail_json(msg=f"HTTP GET request encountered an error: {str(e)}")
 
 
 if __name__ == '__main__':
