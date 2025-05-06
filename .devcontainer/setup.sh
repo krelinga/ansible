df81f664983865cd3276a20e94798a30ed6aa81c
Andy Kreling <krelinga@gmail.com>
2025-05-06 02:13:23 +0000

    add dependency on beautifulsoup4

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,4 +6,4 @@ echo "Installing ansible-galaxy dependencies..."
 ansible-galaxy install -r requirements.yaml
 echo
 echo "Installing bitwarden python sdk..."
-pipx inject ansible-core bitwarden-sdk proxmoxer requests passlib "git+https://github.com/truenas/api_client.git"
+pipx inject ansible-core bitwarden-sdk proxmoxer requests passlib "git+https://github.com/truenas/api_client.git" beautifulsoup4
