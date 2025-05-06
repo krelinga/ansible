a323b746cb28933306dcbd58c977fecdbde94843
Andy Kreling <krelinga@gmail.com>
2025-05-06 20:23:41 +0000

    use a regular requirements.txt file.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,5 +5,5 @@ set -e
 echo "Installing ansible-galaxy dependencies..."
 ansible-galaxy install -r requirements.yaml
 echo
-echo "Installing bitwarden python sdk..."
-pipx inject ansible-core bitwarden-sdk proxmoxer requests passlib "git+https://github.com/truenas/api_client.git" beautifulsoup4
+echo "injecting python requirements into ansible-core..."
+pipx inject ansible-core -r requirements.txt
